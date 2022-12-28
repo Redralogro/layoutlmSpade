f1bc5eaafa26b0ca429927604a0ee67c58e1fa55
longvd <longvd@hiworld.com>
2022-12-28 18:18:06 +0700

    Change optim setting

--- py_train.py
+++ py_train.py
@@ -8,15 +8,38 @@ import torch.nn as nn
 from dataModel.datamd_ import DpDataSet
 from jsonmerge import merge
 from graph_stuff import get_strings, get_qa
+import networkx as nx
+from jsonmerge import merge
+from datetime import datetime
+
 model = LayoutLMModel.from_pretrained("microsoft/layoutlm-base-uncased").cuda()
 config = AutoConfig.from_pretrained("microsoft/layoutlm-base-uncased")
-optimizer = optim.Adam(model.parameters(), lr=1e-4, weight_decay=0.00005)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/layoutlm-base-uncased")
+
+lr = 1e-4
+optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=0.00005)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)  
 trainData = DpDataSet(path='./data/processed/data_train.jsonl')
 train_loader =  DataLoader(trainData, batch_size= 1, shuffle= False, num_workers= 4)
+val_loader =  DataLoader(trainData, batch_size= 1, shuffle= False, num_workers= 1)
 from spade_model import RelationTagger
 
 reduce_size = 256
 ln = nn.Linear(config.hidden_size, reduce_size).cuda()
+
+def extend_matrix(matrix):
+
+    matrix_s = [ [0] + list(x) + [0] for x in list(matrix)] 
+    t_m =  list( np.zeros_like(matrix_s[0]))
+    _s = [t_m] + list(matrix_s) + [t_m]
+
+    return np.array(_s,dtype='int8')
+# np.array(_s)
+def extend_label(label_):
+    label = [ [0] + list(x) + [0] for x in list(label_)]
+    return np.array (label, dtype='int8')
+
+
 def reduce_shape(last_hidden_state, maps):
     i = 0
     # reduce = torch.zeros(768)
@@ -50,7 +73,7 @@ rel_g = RelationTagger(
 dropout = nn.Dropout(0.1).cuda()
 
 def grouth_truth(text_,label_):
-    graph_s = np.array(label_, dtype='int8')
+    graph_s = np.array(label_, dtype='int')
     # print(graph_s.shape)
     label = graph_s[0, :3, :]
     S_ = graph_s[0, 3:, :]
@@ -71,15 +94,19 @@ def grouth_truth(text_,label_):
     return(resul_)
 
 
-for epoch in tqdm(range(0,10)):
+loss_clss = nn.CrossEntropyLoss(weight=torch.tensor([0.1,1]).cuda())
+
+epochs = 2000
+for epoch in tqdm(range(epochs)):
     model.train()
+    
     for i, sample_batched in enumerate((train_loader)):
         input_ids = sample_batched["input_ids"].squeeze(0).cuda()
         attention_mask = sample_batched["attention_mask"].squeeze(0).cuda()
         token_type_ids = sample_batched["token_type_ids"].squeeze(0).cuda()
         bbox = sample_batched["bbox"].squeeze(0).cuda()
         maps = sample_batched['maps']
-        print(input_ids.shape,attention_mask.shape,bbox.shape,token_type_ids.shape, np.array (maps).shape)
+        # print(input_ids.shape,attention_mask.shape,bbox.shape,token_type_ids.shape, np.array (maps).shape)
         optimizer.zero_grad()
         outputs = model(
             input_ids=input_ids, bbox=bbox, attention_mask=attention_mask, token_type_ids=token_type_ids
@@ -87,91 +114,154 @@ for epoch in tqdm(range(0,10)):
         last_hidden_state, maps = outputs.last_hidden_state, maps
         last_hidden_state = ln(last_hidden_state)
         reduce = reduce_shape(last_hidden_state, maps)
-        loss_clss = nn.CrossEntropyLoss()
+        
         #s part
         S = rel_s(dropout(reduce.unsqueeze(0)))
-        # S = rel_s
-        # S = torch.argmax(rel_s,dim =1)
         s0,s1 = S[:,:,:3,:],S[:,:,3:,:]
-        # torch.argmax(s0,dim =1).numpy()
-        s1 =  s1[:,:,1:-1,1:-1]#reduce
-        s0 = s0[:,:,:,1:-1]# reduce
+        graph = np.array(sample_batched['label'])
+        #group part
+        G = rel_g(dropout(reduce.unsqueeze(0)))
+        g0,g1 = G[:,:,:3,:],G[:,:,3:,:]
 
-        
-        graph = torch.tensor(sample_batched['label']).cuda()
-        # graph.copy()
-        label = graph[0, :3, :].unsqueeze(0)
-        # graph = np.array(label)
-
-        
+        # label = graph[0, :3, :].unsqueeze(0)
         # matrix_s = graph[0, 3:, :].unsqueeze(0)
         # matrix_g = graph[1, 3:, :].unsqueeze(0)
-        # loss_label_s = loss_clss(s0, label.long())
 
-        G = rel_g(dropout(reduce.unsqueeze(0)))
-        # rel_s.shape
-        # G = rel_g
-        # S = torch.argmax(rel_s,dim =1)
-        g0,g1 = G[:,:,:3,:],G[:,:,3:,:]
-        # torch.argmax(s0,dim =1).numpy()
-        g1 =  g1[:,:,1:-1,1:-1]#reduce
-        g0 = g0[:,:,:,1:-1]# reduce
-        # graph = torch.tensor(batch['label']).cuda()
-        # label = graph[0, :3, :].unsqueeze(0)
-        # graph = np.array(label)
+        label_s = torch.tensor(extend_label(graph[0, :3, :])).unsqueeze(0).cuda()
+        label_g = torch.tensor(extend_label(graph[1, :3, :])).unsqueeze(0).cuda()
+        matrix_s = torch.tensor(extend_matrix(graph[0, 3:, :])).unsqueeze(0).cuda()
+        matrix_g = torch.tensor(extend_matrix(graph[1, 3:, :])).unsqueeze(0).cuda()
 
-        matrix_s = graph[0, 3:, :].unsqueeze(0)
-        matrix_g = graph[1, 3:, :].unsqueeze(0)
-        loss_label_s = loss_clss(s0, label.long())
+
+        loss_label_s = loss_clss(s0, label_s.long())
+        loss_label_g = loss_clss(g0, label_g.long())
         loss_matrix_s = loss_clss(s1,matrix_s.long())
         loss_matrix_g = loss_clss(g1,matrix_g.long())
-        loss = loss_label_s + loss_matrix_s + loss_matrix_g
+        loss = loss_label_s + loss_matrix_s + loss_label_g + loss_matrix_g
         loss.backward()
-        loss_matrix_s.backward()
-        loss_matrix_g.backward()
+        # loss_matrix_s.backward()
+        # loss_matrix_g.backward()
         print ('loss', loss.detach())
         print ('loss_label_s', loss_label_s.detach())
+        print ('loss_label_g', loss_label_g.detach())
         print ('loss_matrix_s', loss_matrix_s.detach())
         print ('loss_matrix_g', loss_matrix_g.detach())
         # print()
-        text = [x[0] for x in sample_batched["text"]]
+        text = [tokenizer.cls_token] + [x[0] for x in sample_batched["text"]] + [tokenizer.sep_token]
         # print(text)
         with torch.no_grad():
-            label_s = torch.argmax(s0,dim  =1).squeeze(0).cpu().numpy()
-            pred_s = torch.argmax(s1,dim  =1).squeeze(0).cpu().numpy()
-            pred_g = torch.argmax(g1,dim  =1).squeeze(0).cpu().numpy()
-            S_ = pred_s
-            G_ = pred_g
-            question_heads = [i for i, ele in enumerate(label_s[0]) if ele != 0]
-            answer_heads = [i for i, ele in enumerate(label_s[1]) if ele != 0]
-            header_heads = [i for i, ele in enumerate(label_s[2]) if ele != 0]
-            # S_ = list(s1[:,0, 3:, :].squeeze(0).cpu().numpy())
+            ############[GROUND TRUTH]####################
+            label_actual = label_s.squeeze(0)
+            S_ = extend_matrix(graph[0, 3:, :])
+            G_ = extend_matrix(graph[1, 3:, :])
+            question_heads = [i for i, ele in enumerate(label_actual[0]) if ele != 0]
+            answer_heads = [i for i, ele in enumerate(label_actual[1]) if ele != 0]
+            header_heads = [i for i, ele in enumerate(label_actual[2]) if ele != 0]
+
             ques = get_strings(question_heads, text, S_)
             ans = get_strings(answer_heads, text, S_)
-            print('pred', ques,ans)
-            try:
-                for ques_idx in question_heads:
-                    q_, a_ = get_qa(ques_idx, ques, ans, G_)
-                    print('pred', q_, a_)
-                    # resul_ = merge(resul_, {q_: a_})
-            except Exception:
-                print('something gone wrong')
-                    # print(get_ques_ans(ques_idx, ques,ans))
-            # if(len(ques) == len(ans)):
-            #     resul_ = {}
-            #     for ques_idx in question_heads:
-            #         q_, a_ = get_qa(ques_idx, ques, ans, G_)
-            #         resul_ = merge(resul_, {q_: a_})
-            #         # print(get_ques_ans(ques_idx, ques,ans))
-            #     print('pred', resul_)
-            
-            try:
-                grouth_truth(text,sample_batched['label'])
-            except Exception:
-                print(Exception)
-            # print([i for i, ele in enumerate(pred[0]) if ele != 0])
-            # print(pred.shape)
-        # print(outputs.last_hidden_state)
+            print(len(ques), len(ans))
+            print(f'[GROUND TRUTH]: Ques:{ques} \n Ans: {ans}')
+
+            print('\n ###############################')
+            ############[PREDICT]####################
+
+            pred_label = torch.argmax(s0,dim=1).squeeze(0)
+
+            pred_matrix_s = torch.softmax(s1,dim=1)
+            pred_matrix_s = torch.argmax(pred_matrix_s,dim=1).squeeze(0)
+            pred_matrix_g = torch.softmax(g1,dim=1)
+            pred_matrix_g = torch.argmax(pred_matrix_g,dim=1).squeeze(0)
+            # pred_label.shape
+            # pred_matrix_s
+            pred_question_heads = [i for i, ele in enumerate(pred_label[0]) if ele != 0]
+            pred_answer_heads = [i for i, ele in enumerate(pred_label[1]) if ele != 0]
+            pred_header_heads = [i for i, ele in enumerate(pred_label[2]) if ele != 0]
+
+            pred_S = np.array([list(x) for x in np.array(pred_matrix_s.cpu().numpy())])
+            pred_G = np.array([list(x) for x in np.array(pred_matrix_g.cpu().numpy())])
+            # G = nx.Graph(pred_S)
+
+            pred_ques = get_strings(pred_question_heads, text, pred_S)
+            # print(np.shape(ques))
+
+            pred_ans = get_strings(pred_answer_heads, text, pred_S)
+            # print(np.shape(ans))
+            print(f'[PREDICT]: Ques:{pred_ques} \n Ans: {pred_ans}')
+
+            for ques_idx in pred_question_heads:
+                G_pred = nx.Graph(pred_G) # group
+                dfs = list(nx.dfs_edges(G_pred, source=int(ques_idx)))
+                # print(dfs)
+                if len(dfs) !=0:
+                    q,a = dfs[0]
+                    qu_s =  [qs[1] for qs in pred_ques if q in qs ]
+                    an_s=  [as_[1] for as_ in pred_ans if a in as_ ]
+                    # if len(qu_s)== len(an_s):
+                    #     print(qu_s[0], an_s[0])
+                    print( f'[PREDICT]: ques: {qu_s} \n ans: {an_s}' )
+                    
+                    # if len(qu_s) !=0:
+                    #     print("QUES",qu_s[0], "\n")
+                    # else : print("QUES",qu_s, "\n")
+                    # if len(an_s) !=0:
+                    #     print("ANS",an_s[0], "\n")
+                    # else : print("ANS",an_s, "\n")
+
         optimizer.step()
+    scheduler.step()
+    if (epoch + 1) % 10 == 0:
+        lr *= 0.5
+    
+    # if epoch % 5 == 0:    # test every 5 epochs  
+    #     model.eval()
+    #     # with torch.no_grad():
+    #     for i, val_batched in enumerate((val_loader)):
+    #         input_ids = val_batched["input_ids"].squeeze(0).cuda()
+    #         attention_mask = val_batched["attention_mask"].squeeze(0).cuda()
+    #         token_type_ids = val_batched["token_type_ids"].squeeze(0).cuda()
+    #         bbox = val_batched["bbox"].squeeze(0).cuda()
+    #         maps = val_batched['maps']
+    #         outputs = model(
+    #             input_ids=input_ids, bbox=bbox, attention_mask=attention_mask, token_type_ids=token_type_ids
+    #         )
+    #         last_hidden_state, maps = outputs.last_hidden_state, maps
+    #         last_hidden_state = ln(last_hidden_state)
+    #         reduce = reduce_shape(last_hidden_state, maps)
 
+    #             #s part
+    #         S_val = rel_s(dropout(reduce.unsqueeze(0)))
+    #         val_s0,val_s1 = S_val[:,:,:3,:],S_val[:,:,3:,:]
+    #         graph = np.array(val_batched['label'])
+    #         #group part
+    #         G_val = rel_g(dropout(reduce.unsqueeze(0)))
+    #         val_g0,val_g1 = G_val[:,:,:3,:],G_val[:,:,3:,:]
+    #         val_text = [tokenizer.cls_token] + [x[0] for x in val_batched["text"]] + [tokenizer.sep_token]
+    #         with torch.no_grad():
+    #             val_pred_label = torch.argmax(val_s0,dim=1).squeeze(0)
+    #             val_pred_matrix_s = torch.softmax(val_s1,dim=1)
+    #             val_pred_matrix_s = torch.argmax(val_pred_matrix_s,dim=1).squeeze(0)
+    #             val_pred_matrix_g = torch.softmax(val_g1,dim=1)
+    #             val_pred_matrix_g = torch.argmax(val_pred_matrix_g,dim=1).squeeze(0)
+    #             # pred_label.shape
+    #             # pred_matrix_s
+    #             val_pred_question_heads = [i for i, ele in enumerate(val_pred_label[0]) if ele != 0]
+    #             val_pred_answer_heads = [i for i, ele in enumerate(val_pred_label[1]) if ele != 0]
+    #             val_pred_header_heads = [i for i, ele in enumerate(val_pred_label[2]) if ele != 0]
 
+    #             val_pred_S = np.array([list(x) for x in np.array(val_pred_matrix_s.cpu().numpy())])
+    #             val_pred_G = np.array([list(x) for x in np.array(val_pred_matrix_g.cpu().numpy())])
+    #             # G = nx.Graph(pred_S)
+
+    #             val_pred_ques = get_strings(val_pred_question_heads, val_text, val_pred_S)
+    #             # print(np.shape(ques))
+
+    #             val_pred_ans = get_strings(val_pred_answer_heads, val_text, val_pred_S)
+    #             # print(np.shape(ans))
+    #             print(f'[PREDICT VAL]: Ques:{val_pred_ques} \n Ans: {val_pred_ans}')
+
+    if epoch % 20 ==0:
+        now = datetime.now()
+        now = now.strftime("%d-%m-%Y_%H-%M-%S")
+        with open(f"resources/checkpoints/DP_model{now}.pt", "wb") as f:
+            torch.save(model.state_dict(), f)
