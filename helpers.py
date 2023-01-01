5c58a9bf0b691384a4be0c0b829a212bfc2f0acd
Grooo RND <grooornd@hiworld.com.vn>
2023-01-01 18:20:13 +0700

    ƯIP

--- helpers.py
+++ helpers.py
@@ -1,4 +1,4 @@
-from graph_stuff import *  
+from graph_stuff import *
 from traceback import print_exc
 import numpy as np
 import torch.nn as nn
@@ -6,7 +6,7 @@ from typing import Dict
 import json
 import yaml
 
-dropout = nn.Dropout(0.1)
+
 class RelationTagger(nn.Module):
     def __init__(self, n_fields, hidden_size, head_p_dropout=0.1):
         super().__init__()
@@ -41,62 +41,36 @@ class RelationTagger(nn.Module):
         return score
 
 
-# def b_loss(graph_matrix,graph_matrix_pred,ex_bboxes,list_heads):
-#     question_heads, answer_heads,pred_question_heads ,pred_answer_heads  = list_heads
-#     def bbox_loss(graph,ex_bboxes,heads):
-#         temp = []
-#         G = nx.Graph(graph) # s
-#         for index in heads:
-#             dfs = list(nx.dfs_edges(G, source=int(index)))
-#             dfs
-#             if  dfs == []:
-#                 header = [int(index)]
-#             else: header =  [dfs[0][0]] + [x[1]  for i,x in enumerate (dfs)]
-#             list_temp = []
-#             for i in header:
-#                 [x1,y1,x2,y2] = ex_bboxes[int(i)]
-#                 # print(abs(x2 -x1) + abs(y2 -y1) )
-#                 list_temp.append(abs(x2 -x1) + abs(y2 -y1))
-#             temp.append(sum(list_temp)/len(list_temp))
-#         try: 
-#             return sum(temp)/len(temp)
-#         except ZeroDivisionError:
-#             return 0
-    
-#     que_loss = bbox_loss(graph_matrix, ex_bboxes,question_heads)
-#     ans_loss = bbox_loss(graph_matrix, ex_bboxes,answer_heads)
-#     pred_ques_loss =  bbox_loss(graph_matrix_pred, ex_bboxes,pred_question_heads)
-#     pred_ans_loss =  bbox_loss(graph_matrix_pred, ex_bboxes,pred_answer_heads)
-#     sum_loss = abs(pred_ques_loss - que_loss) +abs(pred_ans_loss - ans_loss)
-#     return torch.tensor(sum_loss, requires_grad=True)
+def b_loss(graph_matrix, graph_matrix_S, ex_bboxes, list_heads):
+    question_heads, answer_heads, pred_question_heads, pred_answer_heads = list_heads
 
-def b_loss(graph_matrix,graph_matrix_S,ex_bboxes,list_heads):
-    question_heads, answer_heads,pred_question_heads ,pred_answer_heads  = list_heads
-    def bbox_loss(graph,ex_bboxes,heads):
+    def bbox_loss(graph, ex_bboxes, heads):
         temp = []
-        G = nx.Graph(graph) # s
+        G = nx.Graph(graph)  # s
         for index in heads:
             dfs = list(nx.dfs_edges(G, source=int(index)))
             dfs
-            if  dfs == []:
+            if dfs == []:
                 header = [int(index)]
-            else: header =  [dfs[0][0]] + [x[1]  for i,x in enumerate (dfs)]
+            else:
+                header = [dfs[0][0]] + [x[1] for i, x in enumerate(dfs)]
             list_temp = []
             for i in header:
-                [x1,y1,x2,y2] = ex_bboxes[int(i)]
-                list_temp.append(abs(x2 -x1) + abs(y2 -y1))
+                [x1, y1, x2, y2] = ex_bboxes[int(i)]
+                list_temp.append(abs(x2 - x1) + abs(y2 - y1))
             temp.append(sum(list_temp)/len(list_temp))
-        try: 
+        try:
             return sum(temp)/len(temp)
         except ZeroDivisionError:
             return 0
-    
-    que_loss = bbox_loss(graph_matrix, ex_bboxes,question_heads)
-    ans_loss = bbox_loss(graph_matrix, ex_bboxes,answer_heads)
-    pred_ques_loss =  bbox_loss(graph_matrix_S, ex_bboxes,pred_question_heads)
-    pred_ans_loss =  bbox_loss(graph_matrix_S, ex_bboxes,pred_answer_heads)
-    sum_loss = abs(pred_ques_loss - que_loss) +abs(pred_ans_loss - ans_loss)
+
+    que_loss = bbox_loss(graph_matrix, ex_bboxes, question_heads)
+    ans_loss = bbox_loss(graph_matrix, ex_bboxes, answer_heads)
+    pred_ques_loss = bbox_loss(graph_matrix_S, ex_bboxes, pred_question_heads)
+    pred_ans_loss = bbox_loss(graph_matrix_S, ex_bboxes, pred_answer_heads)
+    sum_loss = abs(pred_ques_loss - que_loss) + abs(pred_ans_loss - ans_loss)
     return torch.tensor(sum_loss, requires_grad=True)
+
 
 def tensorize(x):
     if isinstance(x, np.ndarray):
@@ -107,6 +81,7 @@ def tensorize(x):
         except Exception:
             print_exc()
             return torch.tensor(x)
+
 
 def normalize_box(box, width, height):
     # x1, y1, x2, y2
@@ -121,194 +96,14 @@ def normalize_box(box, width, height):
 
 def load_config(path: str) -> Dict:
     if path.endswith("yml"):
-        with open(path,"r") as f:
+        with open(path, "r") as f:
             config = yaml.load(stream=f, Loader=yaml.FullLoader)
     elif path.endswith("json"):
-        with open(path,"r") as f:
+        with open(path, "r") as f:
             config = json.load(stream=f)
     elif path.endswith("yaml"):
-        with open(path,"r") as f:
+        with open(path, "r") as f:
             config = yaml.load(stream=f, Loader=yaml.FullLoader)
     else:
         raise "Invalid file format"
     return config
-
-def parse_input(
-    image,
-    words,
-    actual_boxes,
-    tokenizer,
-    config,
-    label,
-    fields,
-    cls_token_box=[0, 0, 0, 0],
-    sep_token_box=[1000, 1000, 1000, 1000],
-    pad_token_box=[0, 0, 0, 0],
-):
-    width, height = image.size
-    boxes = [normalize_box(b, width, height) for b in actual_boxes]
-    if label is not None:
-        label = tensorize(label)
-        token_map = map_token(tokenizer, words, offset=len(fields))
-        rel_s = tensorize(label[0])
-        rel_g = tensorize(label[1])
-        token_rel_s = expand(rel_s, token_map, lh2ft=True,
-                               in_tail=True, in_head=True)
-        token_rel_g = expand(rel_g, token_map, fh2ft=True)
-        label = torch.cat(
-            [token_rel_s.unsqueeze(0), token_rel_g.unsqueeze(0)],
-            dim=0,
-        )
-        print ("-----------label-------")
-        print (label)
-
-    tokens = []
-    token_boxes = []
-    actual_bboxes = []  # we use an extra b because actual_boxes is already used
-    token_actual_boxes = []
-    are_box_first_tokens = []
-    maps = []
-    i = 0
-    for word, box, actual_bbox in zip(words, boxes, actual_boxes):
-        word_tokens = tokenizer.tokenize(word)
-        # print(word_tokens)
-        tokens.extend(word_tokens)
-        token_boxes.extend([box] * len(word_tokens))
-        maps.extend([i]*len(word_tokens))
-        i += 1
-        actual_bboxes.extend([actual_bbox] * len(word_tokens))
-        token_actual_boxes.extend([actual_bbox] * len(word_tokens))
-        are_box_first_tokens.extend([1] + [0] * (len(word_tokens) - 1))
-    print(np.array(maps))
-    # Truncation: account for [CLS] and [SEP] with "- 2".
-    special_tokens_count = 2
-    true_length = config.max_position_embeddings - special_tokens_count
-    if len(tokens) > true_length:
-        tokens = tokens[:true_length]
-        token_boxes = token_boxes[:true_length]
-        token_actual_boxes = token_actual_boxes[:true_length]
-        actual_bboxes = actual_bboxes[:true_length]
-        are_box_first_tokens = are_box_first_tokens[:true_length]
-        if label is not None:
-            label = label[:, : (len(fields) + true_length), :true_length]
-
-    # add [SEP] token, with corresponding token boxes and actual boxes
-    tokens += [tokenizer.sep_token]
-    token_boxes += [sep_token_box]
-    actual_bboxes += [[0, 0, width, height]]
-    token_actual_boxes += [[0, 0, width, height]]
-    are_box_first_tokens += [1]
-    # use labels for auxilary result
-    if label is not None:
-        n, i, j = label.shape
-        labels = torch.zeros((n, i + 1, j + 1), dtype=label.dtype)
-        labels[:, :i, :j] = label
-        label = labels
-
-    segment_ids = [0] * len(tokens)
-
-    # print("----")
-    # edge_0 = []
-    # for (i, j) in zip(*torch.where(token_rel_s)):
-    #     l = [(fields + tokens)[i], tokens[j]]
-    #     print(l)
-    #     edge_0.append(" -> ".join(l))
-
-    # next: [CLS] token
-    tokens = [tokenizer.cls_token] + tokens
-    token_boxes = [cls_token_box] + token_boxes
-    actual_bboxes = [[0, 0, width, height]] + actual_bboxes
-    token_actual_boxes = [[0, 0, width, height]] + token_actual_boxes
-    segment_ids = [1] + segment_ids
-    are_box_first_tokens = [2] + are_box_first_tokens
-    # This is tricky because cls need to be inserted
-    # after the labels
-    if label is not None:
-        nfields = len(fields)
-        top_half = label[:, :nfields, :]
-        bottom_half = label[:, nfields:, :]
-        n, i, j = label.shape
-        new_label = torch.zeros(n, i + 1, j + 1, dtype=label.dtype)
-        new_label[:, :nfields, 1:] = top_half
-        new_label[:, (nfields + 1):, 1:] = bottom_half
-        label = new_label
-
-    #     print("----")
-    #     print("AFter CLS")
-    #     edge_1 = []
-    #     for (i, j) in zip(*torch.where(label[0])):
-    #         l = [(fields + tokens)[i], tokens[j]]
-    #         print(l)
-    #         edge_1.append(" -> ".join(l))
-    #     print("----")
-
-    #     for (i, j) in zip(edge_0, edge_1):
-    #         print(i, j, i == j)
-
-    input_ids = tokenizer.convert_tokens_to_ids(tokens)
-
-    # The mask has 1 for real tokens and 0 for padding tokens. Only real
-    # tokens are attended to.
-    input_mask = [1] * len(input_ids)
-
-    # Zero-pad up to the sequence length.
-    padding_length = config.max_position_embeddings - len(input_ids)
-    input_ids += [tokenizer.pad_token_id] * padding_length
-    input_mask += [0] * padding_length
-    segment_ids += [tokenizer.pad_token_id] * padding_length
-    token_boxes += [pad_token_box] * padding_length
-    token_actual_boxes += [pad_token_box] * padding_length
-    are_box_first_tokens += [3] * padding_length
-
-    assert len(input_ids) == config.max_position_embeddings
-    assert len(input_mask) == config.max_position_embeddings
-    assert len(segment_ids) == config.max_position_embeddings
-    assert len(token_boxes) == config.max_position_embeddings
-    assert len(token_actual_boxes) == config.max_position_embeddings
-    assert len(are_box_first_tokens) == config.max_position_embeddings
-    
-    # Label parsing
-    if label is not None:
-        n, i, j = label.shape
-        labels = torch.zeros(
-            n,
-            config.max_position_embeddings + len(fields),
-            config.max_position_embeddings,
-            dtype=label.dtype,
-        )
-        labels[:, :i, :j] = label
-        label = labels
-
-    # assert itc_labels.shape[0] == config.max_position_embeddings
-    # assert stc_labels.shape[1] == config.max_position_embeddings
-    # assert stc_labels.shape[2] == config.max_position_embeddings
-
-    # labels = torch.cat(
-    #     [torch.zeros(labels.shape[0], 1, config.max_position_embeddings), labels],
-    #     dim=1,
-    # )
-
-    # The unsqueezed dim is the batch dim for each type
-    if label is not None:
-        return {
-            "text_tokens": tokens,
-            "input_ids": tensorize(input_ids).unsqueeze(0),
-            "attention_mask": tensorize(input_mask).unsqueeze(0),
-            "token_type_ids": tensorize(segment_ids).unsqueeze(0),
-            "bbox": tensorize(token_boxes).unsqueeze(0),
-            "actual_bbox": tensorize(token_actual_boxes).unsqueeze(0),
-            # "itc_labels": itc_labels.unsqueeze(0),
-            # "stc_labels": stc_labels.unsqueeze(0),
-            "labels": tensorize(labels).unsqueeze(0),
-            "are_box_first_tokens": tensorize(are_box_first_tokens).unsqueeze(0),
-        }
-    else:
-        return {
-            "text_tokens": tokens,
-            "input_ids": tensorize(input_ids).unsqueeze(0),
-            "attention_mask": tensorize(input_mask).unsqueeze(0),
-            "token_type_ids": tensorize(segment_ids).unsqueeze(0),
-            "bbox": tensorize(token_boxes).unsqueeze(0),
-            "actual_bbox": tensorize(token_actual_boxes).unsqueeze(0),
-            "are_box_first_tokens": tensorize(are_box_first_tokens).unsqueeze(0),
-        }
--- py_train.py
+++ py_train.py
@@ -12,6 +12,7 @@ import networkx as nx
 from datetime import datetime
 from modeling.layoutlm import LayoutlmEmbeddings
 from helpers import b_loss
+from jsonmerge import merge
 import mlflow
 from mlflow import log_metric, log_param, log_artifacts
 from loss import BboxLoss
