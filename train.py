b28bb85b54f841b61c6ddc4fdc2ede42e96686e7
Long Vu <redralogro@gmail.com>
2023-01-30 16:37:36 +0700

    export last step

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 # To be continue ...
 from typing import Dict
-
+import torch
 import mlflow
 from dataModel.dataLoader import DpDataMoDule
 from dotenv import load_dotenv
@@ -10,6 +10,7 @@ from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import LearningRateMonitor
 from pytorch_lightning.loggers import MLFlowLogger
 
+from datetime import datetime
 load_dotenv()
 
 config: Dict = load_config("main.yaml")
@@ -32,3 +33,9 @@ trainer = Trainer(accelerator='gpu',
 
 DpModel = LitBaseParsing()
 trainer.fit(model=DpModel, datamodule=data_module)
+
+now = datetime.now()
+now = now.strftime("%d-%m-%Y_%H-%M-%S")
+print('Export last .pt')
+with open(f"./resources/checkpoints/DP_model_finetune_{now}.pt", "wb") as f:
+    torch.save(DpModel.state_dict(), f)
